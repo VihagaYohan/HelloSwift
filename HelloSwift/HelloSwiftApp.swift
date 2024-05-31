e81c26b1ab8fcaf0fa981a0a547e2c63b55be288
Vihaga Yohan <35211294+VihagaYohan@users.noreply.github.com>
2024-05-31 19:57:37 +0530

    Initial Commit

---
+++ HelloSwift/HelloSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloSwiftApp.swift
+//  HelloSwift
+//
+//  Created by Yohan on 2024-05-31.
+//
+
+import SwiftUI
+
+@main
+struct HelloSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
