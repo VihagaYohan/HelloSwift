562ecc50a8a62d4f3092d0c25a37dc5a2d212052
Vihaga Yohan <35211294+VihagaYohan@users.noreply.github.com>
2024-05-31 20:26:36 +0530

    Text, VStack, HStack, Image, AsyncImage

--- HelloSwift/ContentView.swift
+++ HelloSwift/ContentView.swift
@@ -10,11 +10,37 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Image("robot")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .clipShape(RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*/25.0/*@END_MENU_TOKEN@*/,
+                                            style: .continuous))
+           
+            AsyncImage(url: URL(string: "https://plus.unsplash.com/premium_photo-1677094310899-02303289cadf?q=80&w=3132&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")!) { image in
+                image.resizable()
+            } placeholder: {
+                ProgressView("Downloading...")
+            }
+
+
+
+            
+            Text("First line")
+                .foregroundStyle(.cyan)
+                .font(.largeTitle)
+                .padding([.top, .bottom], 20)
+            Text("Second line")
+                .foregroundStyle(.green)
+                .font(.title3)
+            Text("Third line")
+            
+            HStack {
+                Text("Left")
+                Text("Right")
+                    .fontWeight(.bold)
+            }
         }
+        .foregroundColor(.orange)
         .padding()
     }
 }
