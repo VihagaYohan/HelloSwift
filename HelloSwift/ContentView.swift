58d0926c0c75432cc3da89db88b211d48d369f73
Vihaga Yohan <35211294+VihagaYohan@users.noreply.github.com>
2024-06-01 18:32:19 +0530

    Code changes to image

--- HelloSwift/ContentView.swift
+++ HelloSwift/ContentView.swift
@@ -18,6 +18,8 @@ struct ContentView: View {
            
             AsyncImage(url: URL(string: "https://plus.unsplash.com/premium_photo-1677094310899-02303289cadf?q=80&w=3132&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")!) { image in
                 image.resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/, style: /*@START_MENU_TOKEN@*/FillStyle()/*@END_MENU_TOKEN@*/)
             } placeholder: {
                 ProgressView("Downloading...")
             }
