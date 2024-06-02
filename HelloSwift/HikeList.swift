bc79383613b15e3f698b877af2cd08c9f259c258
Vihaga Yohan <35211294+VihagaYohan@users.noreply.github.com>
2024-06-02 08:01:09 +0530

    Added  basic navigation to hike list

--- HelloSwift/HikeList.swift
+++ HelloSwift/HikeList.swift
@@ -14,22 +14,37 @@ struct HikeList: View {
         Hike(name: "Tamanwas Fails", photo: "tam", miles: 5)
     ]
     var body: some View {
-        List(hikes) { hike in
-            HStack(alignment: .top) {
-                Image(hike.photo)
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .clipShape(RoundedRectangle(cornerRadius: 10.0, style: .continuous))
-                    .frame(width: 100)
-                VStack(alignment: .leading) {
-                    Text(hike.name)
-                    Text("\(hike.miles.formatted()) miles")
+        NavigationStack {
+            List(hikes) { hike in
+                NavigationLink(value: hike) {
+                    HikeCellView(hike: hike)
                 }
-            }
+            }.navigationTitle("Hikes")
+            .navigationDestination(for: Hike.self) { hike in
+                    Text(hike.name)
+                }
         }
     }
 }
 
 #Preview {
     HikeList()
+}
+
+struct HikeCellView: View {
+    let hike: Hike
+    
+    var body: some View {
+        HStack(alignment: .top) {
+            Image(hike.photo)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .clipShape(RoundedRectangle(cornerRadius: 10.0, style: .continuous))
+                .frame(width: 100)
+            VStack(alignment: .leading) {
+                Text(hike.name)
+                Text("\(hike.miles.formatted()) miles")
+            }
+        }
+    }
 }
---
+++ HikeDetailsScreen.swift
@@ -0,0 +1,18 @@
+//
+//  HikeDetailsScreen.swift
+//  HelloSwift
+//
+//  Created by Yohan on 2024-06-02.
+//
+
+import SwiftUI
+
+struct HikeDetailsScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HikeDetailsScreen()
+}
