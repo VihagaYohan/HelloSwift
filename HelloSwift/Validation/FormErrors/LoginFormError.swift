0a3f8eb4350cfbfb4795068473effdf4a81c9518
Vihaga Yohan <35211294+VihagaYohan@users.noreply.github.com>
2024-06-15 11:18:41 +0530

    Basic form error implementation

---
+++ HelloSwift/Validation/FormErrors/LoginFormError.swift
@@ -0,0 +1,13 @@
+//
+//  LoginFormError.swift
+//  HelloSwift
+//
+//  Created by Yohan on 2024-06-15.
+//
+
+import Foundation
+
+struct LoginFormError {
+    var email: String = ""
+    var password: String = ""
+}
